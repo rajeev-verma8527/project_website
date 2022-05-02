4c262615b1ebef4646d2c907fab303489c80d507
Rajeev Verma <rajeevverma8527@gmail.com>
2022-05-02 22:53:56 +0530

    init

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/contact",methods=["GET","POST"])
+def contact():
+    if request.method == "POST":
+        f = request.form
+        name = f.get("name")
+        email = f.get("email")
+        subject = f.get("subject")
+        message = f.get("message")
+        ## TODO Save data
+        return redirect(url_for('index'))
+    return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
